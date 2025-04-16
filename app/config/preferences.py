381725db599de9719024b97a8f9fc1800a1a7942
churakovra <churakov.main@gmail.com>
2025-04-16 22:43:26 +0300

    add python-dotenv and preferences.py file

---
+++ app/config/preferences.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+DB_NAME = getenv("DB_NAME")
+DB_USER = getenv("DB_USER")
+DB_PASSWORD = getenv("DB_PASSWORD")
+DB_HOST = getenv("DB_HOST")
+DB_PORT = getenv("DB_PORT")
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
