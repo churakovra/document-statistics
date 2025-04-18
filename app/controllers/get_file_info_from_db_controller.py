2161348f5df89ac5716eb0db26bddd7d48632fd5
churakovra <churakov.main@gmail.com>
2025-04-19 01:52:02 +0300

    get word stats response refactor

--- app/controllers/get_file_info_from_db_controller.py
+++ app/controllers/get_file_info_from_db_controller.py
@@ -1,6 +1,6 @@
 from math import log
 
-from sqlalchemy import func, select
+from sqlalchemy import func, select, Sequence, Row
 from sqlalchemy.orm import Session
 
 from app.models.user_files_db_model import UserFiles
@@ -12,7 +12,34 @@ from app.models.words_in_file_db_model import FileWords
 def get_file_info_db(file_id: int, session: Session) -> list[WordStats]:
     res = list()
 
-    words_in_file = session.execute(
+    words_in_file = get_words(file_id, session)
+
+    word_in_files_query = get_word_in_files(file_id, session)
+
+    file_size = get_file_size(file_id, session)
+
+    files_count = get_files_count(file_id, session)
+
+    for _ in range(0, len(words_in_file)):
+        word, words_in_file_count = words_in_file[_]
+        file_count = word_in_files_query[_][0]
+        res.append(
+            WordStats(
+                word=word,
+                words_in_file_count=words_in_file_count,
+                file_size=file_size,
+                file_count=file_count,
+                files_count=files_count,
+                tf=words_in_file_count / file_size,
+                idf=log(files_count / file_count, 10)
+            )
+        )
+
+    return res
+
+
+def get_words(file_id, session) -> Sequence[Row[tuple[str, int]]]:
+    return session.execute(
         select(
             UserWords.word,
             func.count(FileWords.id_word)
@@ -23,7 +50,9 @@ def get_file_info_db(file_id: int, session: Session) -> list[WordStats]:
         .order_by(FileWords.id_word.desc())
     ).all()
 
-    word_in_files_query = session.execute(
+
+def get_word_in_files(file_id, session) -> Sequence[Row[tuple[int]]]:
+    return session.execute(
         select(
             func.count(FileWords.id_file.distinct())
         )
@@ -39,7 +68,9 @@ def get_file_info_db(file_id: int, session: Session) -> list[WordStats]:
         .order_by(FileWords.id_word.desc())
     ).all()
 
-    file_size = session.scalar(
+
+def get_file_size(file_id, session) -> int:
+    return session.scalar(
         select(
             UserFiles.file_size
         )
@@ -47,26 +78,11 @@ def get_file_info_db(file_id: int, session: Session) -> list[WordStats]:
         .group_by(UserFiles.file_size)
     )
 
-    files_count = session.scalar(
+
+def get_files_count(file_id, session) -> int:
+    return session.scalar(
         select(
             func.count(UserFiles.id)
         )
         .where(UserFiles.id == file_id)
     )
-
-    for _ in range(0, len(words_in_file)):
-        word, words_in_file_count = words_in_file[_]
-        file_count = word_in_files_query[_][0]
-        res.append(
-            WordStats(
-                word=word,
-                words_in_file_count=words_in_file_count,
-                file_size=file_size,
-                file_count=file_count,
-                files_count=files_count,
-                tf=words_in_file_count / file_size,
-                idf=log(files_count / file_count)
-            )
-        )
-
-    return res
