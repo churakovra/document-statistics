7b6f00a4dbb6ce427e554db174b8e52f8b3834db
churakovra <churakov.main@gmail.com>
2025-04-20 13:49:45 +0300

    add frontend realization with streamlit framework Update requirements remove html css and js files

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from starlette.requests import Request
+from starlette.middleware.cors import CORSMiddleware
 
 from app.controllers.add_new_file_controller import add_new_file_router
 from app.controllers.get_file_info_controller import get_file_info_router
@@ -12,13 +9,13 @@ app = FastAPI(
     lifespan=init_db
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(add_new_file_router)
 app.include_router(get_file_info_router)
-
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-templates = Jinja2Templates(directory="app/templates")
-
-
-@app.get("/", response_class=HTMLResponse)
-async def upload_page(request: Request):
-    return templates.TemplateResponse("upload.html", {"request": request})
