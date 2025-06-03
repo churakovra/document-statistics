13b3216a2603dd92e725d26eb996a69c69aad6d6
churakovra <churakov.main@gmail.com>
2025-06-03 22:45:45 +0300

    build(db): remove db init

--- app/config/preferences.py
+++ app/config/preferences.py
@@ -2,9 +2,9 @@ from os import getenv
 
 from dotenv import load_dotenv
 
-#db
 load_dotenv()
 
+# db
 DB_NAME = getenv("DB_NAME")
 DB_USER = getenv("DB_USER")
 DB_PASSWORD = getenv("DB_PASSWORD")
@@ -12,6 +12,6 @@ DB_HOST = getenv("DB_HOST")
 DB_PORT = getenv("DB_PORT")
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-#backend
+# backend
 BACK_SERVICE_NAME = getenv("BACK_SERVICE_NAME")
 BACKEND_PORT = getenv("BACKEND_PORT")
--- app/main.py
+++ app/main.py
@@ -2,11 +2,8 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from app import routers
-from app.db.database import init_db
 
-app = FastAPI(
-    lifespan=init_db
-)
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
