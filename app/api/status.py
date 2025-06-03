a7cbbaee29e074fb7ab122c1fd345c5e422c47e7
churakovra <churakov.main@gmail.com>
2025-06-03 22:41:39 +0300

    feature(Status): add realization for status handler

--- app/api/status.py
+++ app/api/status.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
+from app.db.database import get_session
 from app.services.app_service import AppService
 
 router = APIRouter()
 
 @router.get("/status")
-async def get_status():
-    status = AppService.check_status()
-    return {"status": status}
+async def get_status(session: Session = Depends(get_session)):
+    app_service = AppService(session)
+    status = app_service.check_status()
+    return {"status": status.value}
--- app/services/app_service.py
+++ app/services/app_service.py
@@ -1,9 +1,20 @@
+from sqlalchemy.orm import Session
+
 from app.config.version import __version__
+from app.enums.app_enums import Status
+from app.repositories.app_repository import AppRepository
+
 
 class AppService:
-    @staticmethod
-    def check_status():
-        pass
+    def __init__(self, db_session: Session):
+        self.db_session = db_session
+
+    def check_status(self) -> Status:
+        app_repository = AppRepository(self.db_session)
+        db_up = app_repository.connect_to_db()
+        if db_up:
+            return Status.OK
+        return Status.ERROR
 
     @staticmethod
     def get_version() -> str:
