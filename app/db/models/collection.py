68f1cc3fe6706d8debdc82bbbee5c53d480b2dcd
churakovra <churakov.main@gmail.com>
2025-06-05 23:12:18 +0300

    feature(api-v2): add collection ORM

---
+++ app/db/models/collection.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from typing import List
+from uuid import UUID
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.db.models.base import Base
+from app.db.models.collection_documents import CollectionDocuments
+
+
+class Collection(Base):
+    __tablename__ = "collection"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    create_user: Mapped[UUID]
+    create_dt: Mapped[datetime]
+
+    documents: Mapped[List["CollectionDocuments"]] = relationship("CollectionDocuments", back_populates="collection")
